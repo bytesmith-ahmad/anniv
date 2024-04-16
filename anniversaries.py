a5c80899cb0e20dddf8fdd666c3b14bef150614d
bytesmith-ahmad <b-9k@outlook.com>
2024-04-15 20:16:22 -0400

    Added row count

--- anniversaries.py
+++ anniversaries.py
@@ -32,46 +32,62 @@ def main(*args):
     # Parse command-line arguments
     parser = ArgumentParser(
         description='Manipulate anniversaries database',
-        epilog='Text at the bottom of help (Modify with code bin/anni*)')
+        epilog='Text at the bottom of help (Modify with code bin/anni*)',
+        prefix_chars='-+')
     parser.add_argument('id', nargs='?', default=None, type=int, help='Rowid for specific entry') # ? = 0 or 1    argument
+    # DELETE
+    parser.add_argument('--del','-d', nargs='?', dest='delete', help='Signals DELETE')
+    # INSERT & UPDATE
     parser.add_argument('--add','-a', action='store_true', dest='insert', help='Signals INSERT')
     parser.add_argument('--mod','-m', action='store_true', dest='update', help='Signals UPDATE')
-    parser.add_argument('--del','-d', nargs='?', dest='delete', help='Signals DELETE')
     parser.add_argument('--who',  const=None, help='Name for the anniversary')
     parser.add_argument('--date', const=None, help='Date for the anniversary')
     parser.add_argument('--type', const=None, help='Type for the anniversary')
     parser.add_argument('--note', const=None, help='Note for the anniversary')
+    # WHERE
     parser.add_argument('--where','-w', nargs='+', help='WHERE clause for filtering')  # + = 1 or many arguments > list IF provided
     # def extend_where(args,Q): pass
     parser.add_argument('--or',  dest='_or',  nargs='+')
     parser.add_argument('--and', dest='_and', nargs='+')
     # def order       (args,Q): pass
-    parser.add_argument('--ord','-o')
+    parser.add_argument('--ord','-o', action='store_true')
+    #todo ORDER BY ROWID
+    parser.add_argument('-who',  action='store_const', const='DESC', help='Name for the anniversary')
+    parser.add_argument('-date', action='store_const', const='DESC', help='Date for the anniversary')
+    parser.add_argument('-type', action='store_const', const='DESC', help='Type for the anniversary')
+    parser.add_argument('-note', action='store_const', const='DESC', help='Note for the anniversary')
+    #todo parser ORDER BY ROWID
+    parser.add_argument('+who',  action='store_const', const='ASC', help='Name for the anniversary')
+    parser.add_argument('+date', action='store_const', const='ASC', help='Date for the anniversary')
+    parser.add_argument('+type', action='store_const', const='ASC', help='Type for the anniversary')
+    parser.add_argument('+note', action='store_const', const='ASC', help='Note for the anniversary')
     # def limit       (args,Q): pass
     parser.add_argument('--lim','-l')
+    parser.add_argument('--ofs', dest='offset')
+    # For more complex queries
+    parser.add_argument('--sql', nargs='*', help='Should execute passed queries as they are')
 
     # Parse arguments and execute corresponding SQL queries
     #^ Testing only
-    args = parser.parse_args(shplit(""))
+    #* args = parser.parse_args(shplit(""))
     #* args = parser.parse_args(shplit("12"))
     #* args = parser.parse_args(shplit("--add --who ahmad --date 2024-12-03 --type birthday --note 'a lil note'"))
     #* args = parser.parse_args(shplit("--add --who ahmad --date 2024-12-03"))
-    #! args = parser.parse_args(shplit("--add ahmad 2024-12-03"))
-    #* args = parser.parse_args(shplit("37 --mod --note 2025-12-23"))
-    #* args = parser.parse_args(shplit("23 --mod --who updated --date updated --type updated --note 'sdf sdf sdf sdf sdf sdf sdf sdf '"))
+    #! args = parser.parse_args(shplit("--add ahmad 2024-12-03")) failed test
+    #* args = parser.parse_args(shplit("37 --mod --note 2024-12-23"))
+    #* args = parser.parse_args(shplit("24 --mod --who updated --date updated --type updated --note 'sdf sdf sdf sdf sdf sdf sdf sdf '"))
     #* args = parser.parse_args(shplit("--del 23"))
     #* args = parser.parse_args(shplit("--where who = Nabi"))
-    #* args = parser.parse_args(shplit("--where rowid <= 20"))
+    #! args = parser.parse_args(shplit("--where rowid <= 20")) #todo need quotes
     #* args = parser.parse_args(shplit("--where type LIKE b%"))
-    #args = parser.parse_args(shplit("--where who = ahmad --and type = marriage"))
-    #! args = parser.parse_args(shplit("--where who = Nabi --or type = marriage"))
-    #! args = parser.parse_args(shplit("--ord +date"))
-    #! args = parser.parse_args(shplit("--ord -date --lim 3"))
-    #! args = parser.parse_args(shplit("--lim 3"))
-    #! args = parser.parse_args(shplit("--lim 3 --ofs 3"))
+    #* args = parser.parse_args(shplit("--where who = Nabi --and type = marriage"))
+    #* args = parser.parse_args(shplit("--where who = Nabi --or type = marriage"))
+    #* args = parser.parse_args(shplit("--ord -who +date -type +note"))
+    #* args = parser.parse_args(shplit("--lim 3"))
+    #* args = parser.parse_args(shplit("--lim 3 --ofs 4"))
     #^ Testing only
 
-    # args = parser.parse_args() #! activate when done
+    args = parser.parse_args() #todo deactivate when testing
     Q = Query()
 
     if not args:
@@ -84,18 +100,22 @@ def main(*args):
     elif args.delete:
         Q = delete(args.delete)
     elif args.lim:
-        Q = limit(args) # if no order take care of it
+        Q.sortable = True
+        Q = limit(args,Q)
     elif args.ord:
-        Q = order(args)
+        Q = order(args,Q)
         if args.lim: Q = limit(args,Q)
     elif args.where:
         Q = where(args,Q)
         if args._and or args._or: Q = extend_where(args,Q)
         if args.ord: Q = order(args,Q)
         if args.lim: Q = limit(args,Q)
+    elif args.sql:
+        Q = Query(args.sql)
     else:
         Q = select_all()
-    if not (args.ord or args.lim): Q = default_order(Q)
+    
+    Q = sort_by_date(Q) # sorts by date if not already sorted
 
     # Final command
     command = f"sqlite3 -batch {db_filepath} \".mode {Q.mode}\" \"{Q.sql}\""
@@ -107,20 +127,21 @@ def main(*args):
 #~ Functions definitions
 
 class Query:
-    def __init__(self, sql="SELECT rowid, * FROM anniversaries", mode=default_mode, message=None):
+    def __init__(self, sql="SELECT rowid, * FROM anniversaries ", sortable=False, mode=default_mode, message=None):
         self.sql: str = sql
+        self.sortable: bool = sortable
         self.mode: str = mode
         self.msg: str = message
 
-# Default SQL query
-# select_clause = 'SELECT rowid, * FROM anniversaries '
-# order_clause = 'ORDER BY date ASC'
-
 def select_all():
-    return Query()
+    sql = "SELECT rowid, * FROM anniversaries; SELECT COUNT(*) AS 'rows' FROM anniversaries;"
+    return Query(sql,True)
 
 def select_by_id(_id):
-    return Query(f"SELECT rowid, * FROM anniversaries WHERE rowid = {_id} ",'line')
+    return Query(
+        sql=f"SELECT rowid, * FROM anniversaries WHERE rowid = {_id} ",
+        sortable=True,
+        mode='line')
 
 def insert(args):
     who  = f"'{args.who }'" if args.who  else 'NULL'
@@ -128,9 +149,9 @@ def insert(args):
     typ  = f"'{args.type}'" if args.type else 'NULL'
     note = f"'{args.note}'" if args.note else 'NULL'
     sql  = f"INSERT INTO anniversaries VALUES ({who},{date},{typ},{note});"
-    sql += f"SELECT last_insert_rowid() AS 'Successfully created row';"
+    sql += f"SELECT last_insert_rowid() AS 'Successfully inserted row';"
     mode = 'line'
-    return Query(sql,mode)
+    return Query(sql,False,mode)
 
 def update(args):
     columns = ['who', 'date', 'type', 'note']
@@ -140,16 +161,18 @@ def update(args):
     for i in range(4):
         if values[i]: stmts += [f"{columns[i]} = '{values[i]}'"]
 
-    return Query(f"UPDATE anniversaries SET {','.join(stmts)} WHERE rowid = {args.id}; SELECT changes()", 'line')
+    return Query(sql=f"UPDATE anniversaries SET {','.join(stmts)} WHERE rowid = {args.id}; SELECT changes() AS 'Number of rows updated'",
+                 mode='line')
 
 def delete(_id):
-    return Query(f"DELETE FROM anniversaries WHERE rowid = {_id}; SELECT changes()",'line')
+    return Query(sql=f"DELETE FROM anniversaries WHERE rowid = {_id}; SELECT changes() AS 'Number of rows deleted'",
+                 mode='line')
 
 def where(args,Q):
-    sql = Q.sql
     cond = args.where
-    sql += f" WHERE {cond[0]} {cond[1]} '{cond[2]}'"
-    return Query(sql)
+    Q.sql += f" WHERE {cond[0]} {cond[1]} '{cond[2]}'"
+    Q.sortable = True
+    return Q
 
 def extend_where(args,Q):
     if args._and:
@@ -158,12 +181,31 @@ def extend_where(args,Q):
         Q.sql += f" OR {args._or[0]} {args._or[1]} '{args._or[2]}'"
     return Q
 
-def order       (args,Q): pass
-def default_order(Q):
-    Q.sql += " ORDER BY date ASC"
+def order(args,Q):
+    Q.sortable = False
+    ordering_terms = []
+    if args.who:  ordering_terms += [f"who {args.who}"]
+    if args.date: ordering_terms += [f"date {args.date}"]
+    if args.type: ordering_terms += [f"type {args.type}"]
+    if args.note: ordering_terms += [f"note {args.note}"]
+    if ordering_terms:
+        Q.sql += "ORDER BY " + ", ".join(ordering_terms)
     return Q
-def limit       (args,Q): pass
-def run         (cmds,Q): pass
+
+def limit(args,Q):
+    if Q.sortable:
+        Q = sort_by_date(Q)
+    Q.sql += f"LIMIT {args.lim}"
+    if args.offset:
+        Q.sql += f" OFFSET {args.offset}"
+    return Q
+
+def sort_by_date(Q):
+    if Q.sortable:
+        Q.sql += " ORDER BY date ASC "
+        Q.sortable = False # to avoid adding ORDER BY twice
+    return Q
+
 def run(cmds):
     cmds = shplit(cmds)
     run_in_bash(cmds)
